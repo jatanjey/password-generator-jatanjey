b92113cad02cdcc0103a06dab68a798da76b64b9
Jatan Jeyachandren <jatanjey@gmail.com>
2023-08-11 21:49:07 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # password-generator-jatanjey
+
+allows you to make a password using javascript, css, and html
+
+## Description
+
+As an employee with access to sensitive data, the Password Generator application was created to enable users to generate strong passwords that provide greater security. The application prompts users for password criteria, including length and character types, and then generates a random password that matches the selected criteria.
+
+## Demo
+
+![Password Generator Screenshot](./Assets/images/password-generator.jpg)
+
+## Features
+
+- Generate random passwords based on user-selected criteria.
+- Choose password length between 8 and 128 characters.
+- Select character types to include: lowercase, uppercase, numeric, and special characters.
+- Validates user inputs and ensures at least one character type is selected.
+- can be used on diffrent devices
+
+## Installation
+
+- Clone the repository to your local machine using the following command:
+git clone https://github.com/jatanjey/password-generator-jatanjey.git
+
+- Open the project directory in your code editor.
+
+## Usage
+
+- Open the `index.html` file in a web browser.
+- Click the "Generate Password" button to initiate the password generation process.
+- Follow the prompts to select password criteria (length and character types).
+- The generated password will be displayed in the designated area.
+
+## credit
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN
---
+++ assets/script.js
@@ -0,0 +1,55 @@
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  // Define character sets
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*()_-+=<>?";
+
+  // Ask user for password criteria
+  var passwordLength = parseInt(prompt("Enter password length (between 8 and 128):"));
+
+  // Check password length
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Please enter a valid password length.");
+    return "";
+  }
+
+  var includeLowercase = confirm("Include lowercase characters?");
+  var includeUppercase = confirm("Include uppercase characters?");
+  var includeNumeric = confirm("Include numeric characters?");
+  var includeSpecial = confirm("Include special characters?");
+
+  // Check at least one character type is selected
+  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+    alert("At least one character type must be selected.");
+    return "";
+  }
+
+  // Combine selected character sets
+  var charSet = "";
+  if (includeLowercase) charSet += lowercaseChars;
+  if (includeUppercase) charSet += uppercaseChars;
+  if (includeNumeric) charSet += numericChars;
+  if (includeSpecial) charSet += specialChars;
+
+
+  var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * charSet.length);
+    password += charSet.charAt(randomIndex);
+  }
+
+  return password;
+}
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
